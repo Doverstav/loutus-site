a0845c10eb502b17fbc1c68ad01d6bec6a776a06
Doverstav <pontus.doverstav@kits.se>
2020-04-22 16:06:47 +0200

    Shuffle things around, add max-width to page

--- src/components/TopGradient.vue
+++ src/components/TopGradient.vue
@@ -1,13 +1,18 @@
 <template>
   <div class="top-gradient">
-    <img class="loutus-logo" src="../assets/loutus_logo.svg" />
+    <div class="top-gradient-container">
+      <img class="loutus-logo" src="../assets/loutus_logo.svg" />
+      <p
+        class="top-gradient-text"
+      >We create digital products that are totally awesome. We create digital products that are totally awesome. We create digital products that are totally awesome.</p>
+    </div>
   </div>
 </template>
 
 <script>
 export default {
   name: "TopGradient"
-}
+};
 </script>
 
 <style scoped>
@@ -17,16 +22,36 @@ export default {
   left: 0;
   height: 100%;
   width: 100%;
-  background-image: linear-gradient(170deg, white 50%, rgba(0,0,0,0) 50%);
+  background-image: linear-gradient(170deg, white 50%, rgba(0, 0, 0, 0) 50%);
 
   z-index: 1;
 }
 
+.top-gradient-container {
+  height: 100%;
+  max-width: 1080px;
+  margin: auto;
+  position: relative;
+}
+
 .loutus-logo {
   position: absolute;
+  left: 32px;
   top: 25%;
-  left: 2em;
+  float: left;
 
   max-width: 100%;
+}
+
+.top-gradient-text {
+  position: absolute;
+  bottom: 10%;
+  left: 2em;
+
+  max-width: 350px;
+  text-align: start;
+
+  font-family: Courier New, Courier, Lucida Sans Typewriter, Lucida Typewriter,
+    monospace;
 }
 </style>
