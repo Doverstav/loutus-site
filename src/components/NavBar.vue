df7bdf05d071042899fca52dd8a5ebdaebd3aac9
Doverstav <pontus.doverstav@kits.se>
2020-04-22 12:34:41 +0200

    Add logo and mock links to navbar

--- src/components/NavBar.vue
+++ src/components/NavBar.vue
@@ -1,5 +1,15 @@
 <template>
-  <div class="nav"></div>
+  <div class="nav">
+
+    <img class="logo-small" src="../assets/logo_small.png" />
+
+    <div class="nav-grid-container">
+      <span class="nav-link">HOME</span>
+      <span class="nav-link">MEET LOUTUS</span>
+      <span class="nav-link">WORK</span>
+      <span class="nav-link">CONTACT</span>
+    </div>
+  </div>
 </template>
 
 <script>
@@ -14,7 +24,27 @@ export default {
   width: 100%;
   background-color: white;
   box-shadow: 0 5px 10px lightgray;
-  position: relative; /* Ensure that navbar is above content and box-shadow is visible */
+
+  display: grid;
+  grid-template-columns: auto auto;
+
+  /* Ensure that navbar is above content and box-shadow is visible */
+  position: relative;
   z-index: 2;
+}
+
+.nav-grid-container {
+  display: grid;
+  grid-template-columns: auto auto auto auto;
+}
+
+.logo-small {
+  height: 75%;
+  margin: 8px;
+}
+
+.nav-link {
+  font-family: 'Maven Pro', sans-serif;
+  align-self: center;
 }
 </style>
