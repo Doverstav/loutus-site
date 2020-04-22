7344bf7781099d9865d9367584320ee381884991
Doverstav <pontus.doverstav@kits.se>
2020-04-22 09:52:33 +0200

    First draft of NavBar

---
+++ src/components/NavBar.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="nav"></div>
+</template>
+
+<script>
+export default {
+  name: "NavBar"
+};
+</script>
+
+<style scoped>
+.nav {
+  height: 10vh;
+  width: 100%;
+  background-color: white;
+  box-shadow: 0 5px 10px lightgray;
+  position: relative; /* Ensure that navbar is above content and box-shadow is visible */
+  z-index: 2;
+}
+</style>
