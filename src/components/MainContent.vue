17af37a3e94cab8af0d4f6f74bd059e57f520134
Doverstav <pontus.doverstav@kits.se>
2020-04-26 12:34:35 +0200

    Add main content page with routing

---
+++ src/components/MainContent.vue
@@ -0,0 +1,34 @@
+<script>
+import DoubleGradient from './DoubleGradientContainer';
+import MeetLoutus from './MeetLoutus';
+import Contact from './Contact';
+import Work from './Work';
+
+export default {
+  name: "MainContent",
+  data () { return {
+    currentRoute: window.location.pathname,
+    routes: {
+      '/': DoubleGradient,
+      '/meet-loutus': MeetLoutus,
+      '/work': Work,
+      '/contact': Contact
+    }
+  }},
+  beforeMount () {
+    window.addEventListener('popstate', () => {
+      console.log("new state!");
+      console.log(window.location.pathname)
+      this.currentRoute = window.location.pathname;
+    })
+  },
+  computed: {
+    ViewComponent () {
+      return this.routes[this.currentRoute] || DoubleGradient;
+    }
+  },
+  render (h) {
+    return h(this.ViewComponent);
+  }
+}
+</script>
