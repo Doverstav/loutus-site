e43dee51f9b3df494d0084dfdf530fca3cd70a2f
Doverstav <pontus.doverstav@kits.se>
2020-04-29 21:41:16 +0200

    First version of profile component

---
+++ src/components/pages/meetLoutus/Profile.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>
+    <img :src="image"/>
+    {{imageText}}
+    {{emoji}}
+    {{text}}
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    image: String,
+    imageText: String,
+    emoji: String,
+    text: String,
+  }
+}
+</script>
