8b3f58937533c2ee6ce9b0112b2cd4ad133e35c1
Doverstav <pontus.doverstav@kits.se>
2020-04-29 22:18:29 +0200

    Update profiles to almost follow the design

--- src/components/pages/meetLoutus/Profile.vue
+++ src/components/pages/meetLoutus/Profile.vue
@@ -1,9 +1,13 @@
 <template>
-  <div>
-    <img :src="image"/>
-    {{imageText}}
-    {{emoji}}
-    {{text}}
+  <div class="profile-container">
+    <div class="profile-inner-container">
+      <div class="profile-picture-container">
+        <img class="profile-picture" :src="image" />
+        <p class="profile-emoji">{{emoji}}</p>
+      </div>
+      <p class="profile-picture-text">{{imageText}}</p>
+      <p class="profile-bio-text">{{text}}</p>
+    </div>
   </div>
 </template>
 
@@ -13,7 +17,52 @@ export default {
     image: String,
     imageText: String,
     emoji: String,
-    text: String,
+    text: String
   }
-}
+};
 </script>
+
+<style scoped>
+.profile-container {
+  position: relative;
+  text-align: left;
+
+  margin-left: 32px;
+  margin-right: 32px;
+}
+
+.profile-inner-container {
+  position: absolute;
+  left: 0;
+  top: 35%;
+}
+
+.profile-picture-container {
+  position: relative;
+}
+
+.profile-emoji {
+  position: absolute;
+  bottom: 0;
+  left: 0;
+
+  font-size: 3em;
+  margin: 0;
+}
+
+.profile-picture {
+  width: 75%;
+  display: block;
+}
+
+.profile-picture-text {
+  font-size: 14px;
+  font-family: Courier New, Courier, Lucida Sans Typewriter, Lucida Typewriter,
+    monospace;
+}
+
+.profile-bio-text {
+  font-size: 12px;
+  font-family: "Maven Pro", sans-serif;
+}
+</style>
