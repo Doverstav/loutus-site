90157701d8fdd5b08c340b52c1551c96d041e946
Doverstav <pontus.doverstav@kits.se>
2020-04-30 21:17:55 +0200

    No longer compute data objects, instead they are static

--- src/components/pages/meetLoutus/MeetLoutus.vue
+++ src/components/pages/meetLoutus/MeetLoutus.vue
@@ -5,8 +5,18 @@
         <p>Here is some text!</p>
       </div>
       <div class="meet-loutus-box profiles">
-        <Profile :image="louData.profile" :imageText="louData.profileText" :emoji="louData.emoji" :text="louData.text" />
-        <Profile :image="tusData.profile" :imageText="tusData.profileText" :emoji="tusData.emoji" :text="tusData.text" />
+        <Profile
+          :image="louData.profile"
+          :imageText="louData.profileText"
+          :emoji="louData.emoji"
+          :text="louData.text"
+        />
+        <Profile
+          :image="tusData.profile"
+          :imageText="tusData.profileText"
+          :emoji="tusData.emoji"
+          :text="tusData.text"
+        />
       </div>
     </div>
   </div>
@@ -17,29 +27,27 @@ import Profile from "./Profile";
 
 export default {
   name: "MeetLoutus",
-  components: {
-    Profile
-  },
-  computed: {
-    louData: function() {
-      return {
+  data() {
+    return {
+      louData: {
         profile: require(`../../../assets/profile_lou.png`),
         profileText: "She's a designer.",
         emoji: "🦖",
         text:
           "This is Louise. She is a dinosaur. But she pretends to be a designer so that people will hire her. People think that dinosaurs can’t do design. And that is true because they are extinct."
-      };
-    },
-    tusData: function() {
-      return {
+      },
+      tusData: {
         profile: require(`../../../assets/profile_tus.png`),
         profileText: "He's a developer.",
         emoji: "🤖",
         text:
           "This is Pontus. He does the blip-blop like a robot, so you can understand why his emoticon is a robot. His habitat is inside the [blip-blop] box. He likes it there."
-      };
-    }
-  }
+      }
+    };
+  },
+  components: {
+    Profile
+  },
 };
 </script>
 
