3bb3b89369d87057478792fc88e68f54ee972e9b
Doverstav <pontus.doverstav@kits.se>
2020-06-16 22:41:45 +0200

    First draft of work page

--- src/components/pages/work/Work.vue
+++ src/components/pages/work/Work.vue
@@ -1,3 +1,48 @@
 <template>
-  <div><p>Work</p></div>
+  <div class="work">
+    <div class="work-container">
+      <div>
+        <h1>Products</h1>
+      </div>
+      <div class="product-card-grid">
+        <Card />
+        <Card />
+        <Card />
+      </div>
+    </div>
+  </div>
 </template>
+
+<script>
+import Card from "./Card";
+
+export default {
+  name: "Work",
+  components: {
+    Card
+  }
+};
+</script>
+
+<style scoped>
+.work {
+  background-image: linear-gradient(160deg, white 75%, #eaeaea 75%);
+}
+
+.work-container {
+  box-sizing: border-box;
+
+  height: 100%;
+  max-width: 1224px;
+  margin: auto;
+  padding: 32px;
+}
+
+.product-card-grid {
+  display: grid;
+  justify-items: center;
+
+  grid-template-columns: 1fr 1fr 1fr;
+  column-gap: 32px;
+}
+</style>
